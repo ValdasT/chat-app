2a758947a14594626487300ea12bdc0a7adb22e4
Valdas-Trakumas <valdas.trakumas@ibm.com>
2021-02-04 11:54:39 +0200

    base url in env

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     'csrf-token': Cookies.get("XSRF-TOKEN"),
